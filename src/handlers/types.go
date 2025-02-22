5f582b5595af60a738752a38cb3bb5e25af10db5
Robert Crandall <robert.crandall@gmail.com>
2025-02-22 10:56:55 -0800

    More debugging

--- src/handlers/types.go
+++ src/handlers/types.go
@@ -10,15 +10,21 @@ type Config struct {
 }
 
 type HostStatus struct {
-	Host      string   `json:"host"`
-	IPv4      []string `json:"ipv4,omitempty"`
-	IPv6      []string `json:"ipv6,omitempty"`
-	Error     string   `json:"error,omitempty"`
-	IsBlocked bool     `json:"is_blocked"`
+	Host          string   `json:"host"`
+	IPv4          []string `json:"ipv4,omitempty"`
+	IPv6          []string `json:"ipv6,omitempty"`
+	UnblockedIPv4 []string `json:"unblocked_ipv4,omitempty"`
+	UnblockedIPv6 []string `json:"unblocked_ipv6,omitempty"`
+	Error         string   `json:"error,omitempty"`
+	IsBlocked     bool     `json:"is_blocked"`
 }
 
 type CheckResponse struct {
-	Status     string       `json:"status"`
-	AllBlocked bool         `json:"all_blocked"`
-	Hosts      []HostStatus `json:"hosts"`
+	Status         string `json:"status"`
+	AllBlocked     bool   `json:"all_blocked"`
+	BlockingRanges struct {
+		IPv4 []string `json:"ipv4"`
+		IPv6 []string `json:"ipv6"`
+	} `json:"blocking_ranges"`
+	Hosts []HostStatus `json:"hosts"`
 }
